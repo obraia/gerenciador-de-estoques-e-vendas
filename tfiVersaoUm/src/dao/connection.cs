f8da1dd0595f8199d2878b849de5ad53541a7924
obraia <bryan22czo@gmail.com>
2020-04-25 20:35:09 -0300

    Adicionando função parece reordenar lista de estoque

---
+++ tfiVersaoUm/src/dao/connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MongoDB.Driver;
+
+namespace tfiVersaoUm.src.dao
+{
+    class Connection
+    {
+        public Connection()
+        {
+            try
+            {
+                var cliente = new MongoClient("mongodb+srv://obraia:<password>@cluster0-2jhg5.mongodb.net/test?retryWrites=true&w=majority");
+                var database = cliente.GetDatabase("loja");
+                var colection = database.GetCollection<IProduto>("produtos");
+            }
+            catch(MongoAuthenticationException e)
+            {
+                MessageBox.Show(e.Message);
+            }       
+        } 
+    }
+}
